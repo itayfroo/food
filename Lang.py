c74fede2e3b844aef9351327c8d859a45dfa2542
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-05-09 13:27:49 +0300

    Update Lang.py

--- Lang.py
+++ Lang.py
@@ -1,28 +1,27 @@
-from googletrans import LANGUAGES, Translator
-import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
-def language_chooser():
-    if 'chosen_language' not in st.session_state:
-        st.session_state.chosen_language = 'en'  # Set default language to English
-    language_options = list(LANGUAGES.values())
-    language_options.remove('english') # Remove English from the list
-    language_options.insert(0, 'english') # Insert English at the beginning
-    default_language = st.session_state.chosen_language
-    default_language_index = language_options.index(default_language) if default_language in language_options else 0
-    new_language = st.selectbox("Choose a language", language_options, index=default_language_index)
-    if new_language != st.session_state.chosen_language:
-        st.session_state.chosen_language = new_language
-        st.cache_resource.clear()
-        st.rerun()
+# Define the URL of the website you want to fetch
+url = "https://shironet.mako.co.il/html/indexes/performers/heb_0_popular.html"
 
+# Step 1: Send a GET request to the website
+response = requests.get(url)
 
+# Step 2: Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Step 3: Parse the HTML content of the webpage
+    soup = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
 
-@st.cache_resource
-def translate_word(word):
-    try:
-        if 'chosen_language' not in st.session_state:
-            st.session_state.chosen_language = 'en'
-        translator = Translator()
-        translated_word = translator.translate(word, dest=st.session_state.chosen_language).text
-        return translated_word
-    except: return word
+    # Step 4: Extract all text from the webpage
+    all_text = soup.get_text()
+
+    # Step 5: Split the text into individual words
+    words = all_text.split()
+    
+
+    # Step 6: Print the individual words
+    for word in words:
+        print(word)
+    print("חןילחגה")
+else:
+    print("Failed to fetch the website:", response.status_code)
